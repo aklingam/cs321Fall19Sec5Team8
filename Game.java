6ef8360397ea501ecafa95911dc13baf5f7595ef
aklingam <aklingam@masonlive.gmu.edu>
2019-11-19 14:03:13 -0500

    final version (not error checked) ( pre voice )

--- Game.java
+++ Game.java
@@ -1,32 +1,22 @@
 //Driver class for the chess game. 
 import java.util.*;
 public class Game {
-    boolean whitesTurn,whiteInCheck,blackInCheck, whiteCastleKing, whiteCastleQueen, blackCastleKing, blackCastleQueen, gameSaved;
-	int movesPlayedWithoutCapture; //30 moves without capture is an automatic draw
+    boolean whitesTurn, gameSaved;
 	int moveCount;
 	Board board;	
 	public Game() {
 		whitesTurn = true;
-		whiteCastleKing = true;
-		whiteCastleQueen = true;
-		blackCastleKing = true;
-		blackCastleQueen = true;
-		gameSaved = true;
+		gameSaved = true; //we wont bother telling the user they havent saved on a new game.
 		movesPlayedWithoutCapture = 0;
 		moveCount = 0;
 		board = new Board();
 	}
-	public Game(String FENString) {
-		StringTokenizer st = new StringTokenizer(" ");
+	public Game(String data) {
+		StringTokenizer st = new StringTokenizer(data," ");
 		String pieces = st.nextToken(); 
 		String turn = st.nextToken();
-		String castleAvail = st.nextToken();
-		st.nextToken(); //In this rep we are not doing enpassant, so we are just throwing that token out.
-		movesPlayedWithoutCapture = Integer.parseInt(st.nextToken());
-		moveCount = st.nextToken();
-	}
-	public void stockfishInit() {
-
+		moveCount = Integer.parseInt(st.nextToken());
+		whitesTurn = turn.equals("w") ? true : false;
 	}
 	public boolean validMove(Move m) {
 		//SANITY CHECKS
@@ -40,36 +30,18 @@ public class Game {
 
 
 
-	//returns an FEN string of the game state for use with the stockfish api.
+	//returns a simplified FEN string to load into file..
 	public String FENString() {
 		Stringbuilder sb = board.boardFENString();
 		String turn = whitesTurn ? " w " : " b "; //the space is needed, dont remove.
 		sb.append(turn);
-		String castling = "";
-		if(whiteCastleKing)  { castling+="K" }	
-		if(whiteCastleQueen) { castling+="Q" }	
-		if(blackCastleKing)  { castling+="k" }	
-		if(blackCastleQueen) { castling+="q" }	
-		if(castling.equals("") { castling = "-"; }	
-		sb.append(castling);
-		sb.append(" -");
-		sb.append(" " + movesPlayedWithoutCapture);
-		sb.append(" " + moveCount);
 		return sb.toString();		
 	}
 
-	//Function to actually make the moves
-	//Return codes:
-	//0: normal move.
-	//1: the move was a capture
-	//2: the move was a castle
-	//3: the move was a pawn promotion
-	//... or something alone those lines, havent gotten to it yet
 	public int makeMove(Move m) {
 		gameSaved = false;
 		board.boardState[m.endPos.row][m.endPos.col] = board.boardState[m.startPos.row][m.startPos.col]; //Put the start pieces data in the end pieces spot.
 		board.boardState[m.startPos.row][m.startPos.col] = new Piece(PieceType.EMPTY,null);	
-
 	}
 	public static void main(String[] args) {
 		Game game;
@@ -82,64 +54,33 @@ public class Game {
 		}
 		Scanner sc = new Scanner(System.in);
 		while(1) {
-			StringTokenizer st = new StringTokenizer(sc.nextLine());
-			switch(st.nextToken().toLower()) {
-				case "move": 			
-					Move m = new Move( parsePosition(st.nextToken()) , 
-									   parsePosition(st.nextToken()) );
-					if(validMove(m)) { 
-						makeMove(m); 
-					} else {
-						System.out.println("Invalid move try again");
-					}
-					if(victor()!=-1) { } //TODO: handle output on someone winning
+			StringTokenizer st = new StringTokenizer(sc.nextLine()," ");
+			ArrayList<String> tokens = new ArrayList<String>();
+			while(st.hasNextToken()) { tokens.add(st.nextToken()); }		
+			if(tokens.size()==0) { break; }
+			switch(tokens.get(0)) { 
+				case "move": 
+					//TODO: check if the move is valid with valid move, and if valid, use makemove.
+					//TODO: also set the game saved boolean to false.
 					break;
 				case "save":
-					if(gameSaved) {
-						break; 
-					}	
-					String s = 	FENString();	
-					//TODO: write to file
+						
+					//TODO: write to file by calling FEN string, and writing result to file
+					//ALSO update the gamesaved boolean.
 					//if user supplied another arg try to save to a file named what they passed, otherwise save over the file they loaded if there is one, and if there isnt one, fail to save and prompt for a file name.	
-				case "load":
-					if(!st.hasNextToken()) {
-						System.out.println("Usage: load filename");
-						break;
-					}
-					String file = st.nextToken(); //Half of this stuff needs to be wrapped in try catches or checked
-					String force = st.nextToken();	
-					if(!gameSaved) {
-						if(force!=null || !force.equals("force")) {
-							
-						} else {
-							System.out.println("Game is not saved, type load -filename- force if you wish to ignore this
-						}
-					}	
-				case "voice": 
-					//TODO: wrap in voice control.
-					Move m; //m would be gotten from some kind of function
-					if(validMove(m)) { 
-						makeMove(m); 
-					} else {
-						System.out.println("Invalid move try again");
-					}
-					if(victor()!=-1) { } //TODO: handle output on someone winning
 					break;	
+				case "load":
+					//TODO: if the user has not saved, tell them to use load -filename- -force to override this
+					//otherwise if they typed force or if they have saved the game, load the filename
+					break;
 				case "exit":
 					//TODO: similar functionality to load, in that if the user has not saved we prompt and ignore the request, but if the user types force afterwards, we do it anyways.
-					System.exit(0); 
+					
+					System.exit(0);
+				break; 
 			}
 		}		
 	
 	}
-
-
-	//returns the victor of the game, -1 = no victor yet, 0 = white, 1 = black.
-	public int victor() {
-		
-		//TODO: //check is the person who would like to move has any valid moves at all, if the king is in check, they lose, if the king is not in check, its a draw.
-		return -1;
-	}
-
 
 }
